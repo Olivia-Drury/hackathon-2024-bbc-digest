d15aea5045de6d571f1e678e0146ea32f582be4f
cheunm01 <manyee.cheung@bbc.co.uk>
2024-12-16 17:29:12 +0000

    [MY]: * BBC Sounds SVG Component * Hook to call full list of favourites data * Dummy Digest Sounds Card

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { DigestCard } from "./components/Card";
+// import { DigestCard } from "./components/DigestCard";
+import { DigestGrid } from "./components/DigestGrid";
+import { dataFetcher } from "./components/dataFetcher";
+
+interface DigestDataModel {
+  id: number,
+  favourite: boolean,
+  category: string,
+  data: {
+    title: string,
+    image: string,
+    uuid: number
+  };
+}
 
 function App() {
+  // useEffect(() => {}, []); -->> This is where we collect the data from the backend - pass the whole json object to the DigestGrid
+  // useState to store the data
+  const [digiestData, setdigiestData] = useState<[]>([]);
+  // await the dataFeatcher function
+  useEffect(() => {
+    const fetchData = async () => {
+      await dataFetcher().then((data) => {
+        if (data) {
+          setdigiestData(data);
+        }
+      })
+    }
+    fetchData();
+  }, []);
+  // console.log(digiestData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <DigestCard />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <DigestCard /> */}
+        <DigestGrid />
       </header>
     </div>
   );
---
+++ apps/frontend/src/components/CardSounds.tsx
@@ -0,0 +1,71 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import * as React from "react";
+import BBCNewsIcon from "./BBCNewsIcon";
+import BBCSoundsIcon from "./BBCSoundsIcon";
+
+// interface ExpandMoreProps extends IconButtonProps {
+//   expand: boolean;
+// }
+
+// const ExpandMore = styled((props: ExpandMoreProps) => {
+//   const { expand, ...other } = props;
+//   return <IconButton {...other} />;
+// })(({ theme }) => ({
+//   marginLeft: "auto",
+//   transition: theme.transitions.create("transform", {
+//     duration: theme.transitions.duration.shortest,
+//   }),
+//   variants: [
+//     {
+//       props: ({ expand }) => !expand,
+//       style: {
+//         transform: "rotate(0deg)",
+//       },
+//     },
+//     {
+//       props: ({ expand }) => !!expand,
+//       style: {
+//         transform: "rotate(180deg)",
+//       },
+//     },
+//   ],
+// }));
+
+export const CardSounds = () => {
+  //   const [expanded, setExpanded] = React.useState(false);
+
+  //   const handleExpandClick = () => {
+  //     setExpanded(!expanded);
+  //   };
+
+  return (
+    <Card
+      sx={{
+        p: 2,
+        maxWidth: 400,
+      }}
+    >
+      <CardHeader
+        avatar={<BBCSoundsIcon />}
+        action={<IconButton aria-label='settings'></IconButton>}
+        title={
+          <Typography variant='h6'>
+            Councils to be merged in major local government shake-up
+          </Typography>
+        }
+      />
+
+      <CardMedia
+        component='img'
+        height='194'
+        image='https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/3bbc/live/3a0be170-bb11-11ef-a25a-510b123ed954.png.webp'
+        alt='View over Scarborough in North Yorkshire'
+      />
+    </Card>
+  );
+};
