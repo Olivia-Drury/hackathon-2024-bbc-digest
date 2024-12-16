c8609515efa71a8d25ecdd6cc5c3155e716ba8dd
Hani-bbc <hani.samir.bbc@gmail.com>
2024-12-16 15:59:30 +0000

    basic card design using mui

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { DigestCard } from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DigestCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
