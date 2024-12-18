e98a308a636ea60ace155711fe08a82b269cab7c
cheunm01 <manyee.cheung@bbc.co.uk>
2024-12-18 17:17:02 +0000

    Digest Content

--- apps/frontend/src/components/DigestContent.tsx
+++ apps/frontend/src/components/DigestContent.tsx
@@ -1,12 +1,93 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import { iplayerSoundsEpisodeData } from "../models/types";
+import { Box, styled } from "@mui/material";
+import AppIcons from "../utils/AppIcons";
+
+// Dummy implementation of getDigestAppCategory
+const getDigestAppCategory = () => {
+  return "someCategory";
+};
+
+const StyledEpisodeCard = styled(Card)(({ theme }) => ({
+  maxHeight: 300,
+  borderRadius: 0,
+  display: 'flex', // Use flexbox layout
+  flexDirection: 'row',
+  boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+  transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+  '&:hover': {
+    transform: 'translateY(-5px)',
+    boxShadow: '0px 6px 18px rgba(0, 0, 0, 0.2)',
+  },
+}));
 
 const DigestContent = () => {
   const location = useLocation();
   const data = location.state;
 
   console.log("Data from digest content: ", data);
-  return <div>{/* <AppIcons app={getDigestAppCategory} /> */}</div>;
+  return (
+    <div>
+      <Box
+        sx={{
+          display: 'flex', // Flexbox for alignment
+          padding: 2, // Add padding
+        }}
+      >
+        {/* <AppIcons app={getDigestAppCategory} /> */}
+        <Typography
+          sx={{
+            ml: 1,
+          }}
+        >
+          {/* {getDigestAppCategory === 'iplayer'
+            ? 'iPlayer'
+            : getDigestAppCategory.charAt(0).toUpperCase() +
+              getDigestAppCategory.slice(1)} */}
+        </Typography>
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+      </Box>
+      {data.episodes.map((episode: iplayerSoundsEpisodeData) => {
+        return (
+          <StyledEpisodeCard>
+            <Card
+              sx={{
+                height: {
+                  xs: 320,
+                  sm: 320,
+                  md: 350,
+                  lg: 350,
+                },
+                display: 'flex',
+                flexDirection: 'row',
+              }}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  sx={{ width: 151 }}
+                  image={episode.image}
+                  alt={episode.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='div'>
+                    {episode.title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </StyledEpisodeCard>
+        );
+      })}
+    </div>
+  );
 };
 
 export default DigestContent;
